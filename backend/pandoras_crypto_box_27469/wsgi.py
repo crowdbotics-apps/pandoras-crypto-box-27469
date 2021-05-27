2acf8f36b7276c38b80ff44f5adaabb6178db679
Crowdbotics <team@crowdbotics.com>
2021-05-27 11:27:32 +0000

    Update models

--- backend/pandoras_crypto_box_27469/wsgi.py
+++ backend/pandoras_crypto_box_27469/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pandoras_crypto_box_27469.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pandoras_crypto_box_27469.settings")
 
 application = get_wsgi_application()
